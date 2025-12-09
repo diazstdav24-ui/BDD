8f5abae867e986dd37522eeedf5851568a909dd6
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2025-12-09 23:15:03 +0100

    ejercicio5Boletin1

--- consultas/climatologia/consultasEjercicio5.sql
+++ consultas/climatologia/consultasEjercicio5.sql
@@ -22,10 +22,63 @@ from climatologia
 where provincia IN ('A Coruña','Lugo','Pontevedra','Ourense')
  	AND fecha BETWEEN '2019-09-01' AND '2019-09-30';
 
-select fecha, estacion, provincia, precipitacion_total
+select fecha, estacion, provincia, precipitacion_total,
+	Round( case 
+			when precipitacion_total = 0 then 0.00
+			else (precipitacion_0_a_6::numeric/precipitacion_total)*100 end,2) as "Porcentaje_0_6"
+	Round( case 
+			when precipitacion_total = 0 then 0.00
+			else(precipitacion_6_a_12::numeric/precipitacion_total)*100 end,2)as "Porcentaje_6_12"
+	Round( case 
+			when precipitacion_total = 0 then 0.00
+	 		else (precipitacion_12_a_18::numeric/precipitacion_total)*100 end,2)as "Porcentaje_12_16" 
+	Round( case 
+			when precipitacion_total = 0 then 0.00
+			else(precipitacion_18_a_24::numeric/precipitacion_total)*100 end,2)as "Porcentaje_18_24"
+from climatologia
+where provincia in ('Guadalajara','Cuenca','Toledo','Ciudad Real','Albacete')
+ 	and fecha between '2019-3-21'and '2019-6-20'; 
 
-	round((precipitacion_0_a_6::numeric/precipitacion_total)*100,2) as "Porcentaje_0_6",
-	round((precipitacion_6_a_12::numeric/precipitacion_total)*100,2)as "Porcentaje_6_12",
-from climatologia;
-where 
+SELECT
+    fecha,
+    estacion,
+    provincia,
+    precipitacion_total,
+    ROUND(CASE 
+            WHEN precipitacion_total = 0 THEN 0.00
+            ELSE (precipitacion_0_a_6::numeric / precipitacion_total) * 100 END,2 ) AS "Porcentaje_0_6", 
+    ROUND(CASE 
+            WHEN precipitacion_total = 0 THEN 0.00 
+            ELSE (precipitacion_6_a_12::numeric / precipitacion_total) * 100 END,2 ) AS "Porcentaje_6_12",
+    ROUND(CASE 
+            WHEN precipitacion_total = 0 THEN 0.00 
+            ELSE (precipitacion_12_a_18::numeric / precipitacion_total) * 100 END,2 ) AS "Porcentaje_12_18", 
+    ROUND(CASE 
+            WHEN precipitacion_total = 0 THEN 0.00 
+            ELSE (precipitacion_18_a_24::numeric / precipitacion_total) * 100 END, 2 ) AS "Porcentaje_18_24"
+FROM
+    climatologia
+WHERE
+    provincia IN ('Guadalajara','Cuenca','Toledo','Ciudad Real','Albacete')
+    AND fecha BETWEEN '2019-3-21' AND '2019-6-20'; 
+
+	--Aqui me vas a tener que perdonar pero aunque lo tenia igual puesto en mi consulta, me estaba dando error
+	--Así que use el notebook para poder arreglarlo 
+
+
+/*Selecciona todos los datos de aquellas estaciones que han tenido algún día  
+con una precipitación total de más de 50 litros por metro cuadrado, 
+y donde el % de la precipitación caída de 6 a 12 horas sea entre el 60 y el 80%. 
+Ordena la salida por precipitación total descendente, y fecha ascendente*/
+
+select * 
+from climatologia
+where precipitacion_total >50 
+ 	and (case 
+	 	when precipitacion_total = 0 then  0.0
+		 else (precipitacion_6_a_12::numeric / precipitacion_total)
+	end 
+	between 0.60 and 0.80)
+order by precipitacion_total desc, fecha asc;
+
 	
